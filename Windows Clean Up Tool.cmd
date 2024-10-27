969249dd484a966d91ec744b3316e7ef8e96648f
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2024-10-27 18:52:30 +0300

    Update Windows Clean Up Tool.cmd

--- Windows Clean Up Tool.cmd
+++ Windows Clean Up Tool.cmd
@@ -1,5 +1,6 @@
 @echo off
 	del /s /f /q "%SYSTEMROOT%\Recent\*.*"
+	del /s /f /q "%SYSTEMROOT%\prefetch\*.*"
 	del /s /f /q "%SYSTEMROOT%\Temp\*.*"
 	del /s /f /q "%TEMP%\*.*"
 	del /s /f /q "%TMP%\*.*"
