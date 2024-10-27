b03dbe4e99869b54fbb935b750fd5ab239109939
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2024-10-27 18:55:03 +0300

    Delete Windows Clean Up Tool.cmd

--- Windows Clean Up Tool.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-	del /s /f /q "%SYSTEMROOT%\Recent\*.*"
-	del /s /f /q "%SYSTEMROOT%\prefetch\*.*"
-	del /s /f /q "%SYSTEMROOT%\Temp\*.*"
-	del /s /f /q "%TEMP%\*.*"
-	del /s /f /q "%TMP%\*.*"
-	del /s /f /q "TEMP\*.*"
-
-exit
