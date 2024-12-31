f18448096e3cc8cde83ecc8d1544132d29075384
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2024-12-31 17:26:40 +0300

    Update Windows Clean Up Tool.bat

--- Windows Clean Up Tool.bat
+++ Windows Clean Up Tool.bat
@@ -1,9 +1,15 @@
 @echo off
-	del "%SYSTEMROOT%\Recent\*.*" /S /F /Q
-	del "%SYSTEMROOT%\prefetch\*.*" /S /F /Q
-	del "%SYSTEMROOT%\Temp\*.*" /S /F /Q
-	del "%TEMP%\*.*" /S /F /Q
-	del "%TMP%\*.*" /S /F /Q
-	del "TEMP\*.*" /S /F /Q
+	del /Q %SYSTEMROOT%\Recent\*
+	for /D %%x in (%SYSTEMROOT%\Recent\*) do @rd /S /Q "%%x"
+	del /Q %SYSTEMROOT%\prefetch\*
+	for /D %%x in (%SYSTEMROOT%\prefetch\*) do @rd /S /Q "%%x"
+	del /Q %SYSTEMROOT%\Temp\*
+	for /D %%x in (%SYSTEMROOT%\Temp\*) do @rd /S /Q "%%x"
+	del /Q %Temp%\*
+	for /D %%x in (%Temp%\*) do @rd /S /Q "%%x"
+	del /Q %TMP%\*
+	for /D %%x in (%TMP%\*) do @rd /S /Q "%%x"
+	del /Q TEMP\*
+	for /D %%x in (TEMP\*) do @rd /S /Q "%%x"
 
 exit
